624ee5e5dbe21cbdc599032e3498e5864b5e338e
Bilal Wasim <bilalwasim676@gmail.com>
2020-08-22 03:24:26 +0500

    Adding initial code that registers our sensor, and invokes the _init routine. Still to be tested..

--- drivers/sensor/sps30/sps30.c
+++ drivers/sensor/sps30/sps30.c
@@ -6,3 +6,29 @@
 
 #define DT_DRV_COMPAT sensirion_sps30
 
+#include "sps30.h"
+
+LOG_MODULE_REGISTER(sps30, CONFIG_SENSOR_LOG_LEVEL);
+
+static const struct sensor_driver_api sps30_api = {
+	.sample_fetch = /* &si7060_sample_fetch */ NULL,
+};
+
+static int sps30_init(struct device *dev)
+{
+	struct sps30 *drv_data = dev->data;
+
+	drv_data->i2c_dev = device_get_binding(DT_INST_BUS_LABEL(0));
+	if (!drv_data->i2c_dev) {
+		LOG_ERR("Failed to get pointer to %s device!",
+			DT_INST_BUS_LABEL(0));
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static struct sps30 sps30_data;
+
+DEVICE_AND_API_INIT(sps30, DT_INST_LABEL(0), sps30_init,
+		&sps30_data, NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY, &sps30_api);
--- drivers/sensor/sps30/sps30.h
+++ drivers/sensor/sps30/sps30.h
@@ -7,4 +7,13 @@
 #ifndef ZEPHYR_DRIVERS_SENSOR_SPS30_SPS30_H_
 #define ZEPHYR_DRIVERS_SENSOR_SPS30_SPS30_H_
 
+#include <device.h>
+#include <drivers/i2c.h>
+#include <drivers/sensor.h>
+#include <logging/log.h>
+
+struct sps30 {
+	struct device *i2c_dev;
+};
+
 #endif /* ZEPHYR_DRIVERS_SENSOR_SPS30_SPS30_H_ */
