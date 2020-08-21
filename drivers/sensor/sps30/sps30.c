b4ab297814464eaf956e17993057762b91fe6e4f
Bilal Wasim <bilalwasim676@gmail.com>
2020-08-22 02:53:41 +0500

    Adding structure for SPS30 driver, and appropriate kconfig information.

---
+++ drivers/sensor/sps30/sps30.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2020 Bilal Wasim
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT sensirion_sps30
+
---
+++ drivers/sensor/sps30/sps30.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020 Bilal Wasim
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_SPS30_SPS30_H_
+#define ZEPHYR_DRIVERS_SENSOR_SPS30_SPS30_H_
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_SPS30_SPS30_H_ */
